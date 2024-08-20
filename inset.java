e574b07c9af2243c21dcac831ecbaecfcfa791b8
aryan kumar chowdhury <aryanchowdhury872@gmail.com>
2024-08-21 02:07:58 +0530

    Update inset.java

--- inset.java
+++ inset.java
@@ -13,7 +13,7 @@ public class inset {
             // Creating a connection
             String url = "jdbc:mysql://localhost:3306/aryan";
             String username = "root";
-            String password = "7075395094";
+            String password = "(use password of sql)";
             Connection con = DriverManager.getConnection(url, username, password);
 
             //create a query
