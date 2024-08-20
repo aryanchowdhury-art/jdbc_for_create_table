7731c5520c185421fa9e5b97246c4207e68a26b1
aryan kumar chowdhury <aryanchowdhury872@gmail.com>
2024-08-20 07:39:45 +0530

    Add files via upload

---
+++ jdbcapp.java
@@ -0,0 +1,35 @@
+package com.jdbc;
+
+import java.sql.*;
+
+public class jdbcapp {
+    public static void main(String[] args) {
+        // This is our first JDBC project
+        try {
+            // Load the driver to JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Creating a connection
+            String url = "jdbc:mysql://localhost:3306/aryan";
+            String username = "root";
+            String password = "7075395094";
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            // Create a query
+            String q = "create table table1 (tId int(20) NOT NULL AUTO_INCREMENT PRIMARY KEY, tName varchar(200) NOT NULL, tCity varchar(400))";
+
+            // Create statement
+            Statement stmt = con.createStatement();
+
+            // Execute the query
+            stmt.executeUpdate(q);
+            System.out.println("Table created...");
+            System.out.println("hell...fuckin yeah it is created yeeessss...!!!");
+
+            // Close the connection
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
